95694aba78e568730b08f54d049797b5e4987490
leedua <1063809383@qq.com>
2020-11-26 22:25:34 +0800

    a

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 HOME=/home/hadoop/leedua/
 cd ${HOME}mvsbuild
-cmake . ../openMVS -DCMAKE_BUILD_TYPE=Release -DVCG_ROOT=${HOME}vcglib
 
-make -j64
+DEBUG="Release";
+case "$1" in
+  "-d")DEBUG="DEBUG";;
+  *);;
+esac
+
+cmake . ../openMVS -DCMAKE_BUILD_TYPE=${DEBUG} -DVCG_ROOT=${HOME}vcglib
+
+cmake --build . -j64 --target RefineMesh
+cmake --build . -j64 --target ViewFilter
--- run.sh
+++ run.sh
@@ -1,10 +1,29 @@
 HOME=/home/hadoop/leedua/
 BIN=${HOME}mvsbuild/bin/
+FILTER=false
+DEBUG=false
+#INPUT_MVS=/home/hadoop/data/data/1kmMesh/scene_1km.mvs
+RAW_MVS=/home/hadoop/data/data/yq/Mesh/scene_test/scene_mesh.mvs
+INPUT_MVS=/home/hadoop/data/data/yq/Mesh/scene_test/scene_mesh_filtered.mvs
+FILTER_FILE=/home/hadoop/data/data/yq/Mesh/scene_test/pairName.txt
+WD=/home/hadoop/data/data/yq/output_data/undistorted_images
+#WD=/home/hadoop/data/data/yq/output_data_25726/undistorted_images
 
-INPUT_MVS=/home/hadoop/data/data/1kmMesh/scene_1km.mvs
-WD=/home/hadoop/data/data/yq/output_data_25726/undistorted_images
+case "$1" in
+  "-f")FILTER=true;;
+  "-d")DEBUG=true;;
+  *);;
+esac
 
 cd ${BIN}
-./RefineMesh ${INPUT_MVS} --scales 2 -w ${WD}
- 
+if  [ "${FILTER}" == false ];then
+if [ "${DEBUG}" == false ];then
+./RefineMesh ${INPUT_MVS} --scales 2 -w ${WD} >> ${HOME}scripts/out.log
+else
+gdb --args RefineMesh ${RAW_MVS} --scales 2 -w ${WD}
+fi
+else
+./ViewFilter ${RAW_MVS} -w ${WD} -f ${FILTER_FILE} 
+#gdb --args ViewFilter ${INPUT_MVS} -w ${WD} -f ${FILTER_FILE} 
+fi
 cd ${HOME}
